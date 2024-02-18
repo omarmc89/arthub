9617b6a772563d5c5263bb3a116e1c2548ced2ab
Omar Martin <omarmc89@gmail.com>
2024-02-18 19:35:57 +0100

    entrega terminada

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -24,7 +24,7 @@ const submit = () => {
         ...data,
         remember: form.remember ? 'on' : '',
     })).post(route('login'), {
-        onFinish: () => form.reset('password'),
+        onFinish: () =>form.reset('password'), 
     });
 };
 </script>
